dad00963405f11fc2cf0197c9c2cfd8e1c8eb439
Dave Quillman <davequillman@gmail.com>
2025-09-28 10:44:08 -0500

    chore: bump version to v0.1.23

--- config/settings.py
+++ config/settings.py
@@ -78,4 +78,4 @@ GEOCODER_USER_AGENT = "traveler-app"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # App version (surface in navbar). Update on releases.
-APP_VERSION = "v0.1.22"
+APP_VERSION = "v0.1.23"
